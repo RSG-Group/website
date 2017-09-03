eabcfa0c3948e758ae24b41164cdda3275c583a6
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-09-03 13:31:51 +0530

    Add link to logo image in header (Closes #3)

--- imports/header.js
+++ imports/header.js
@@ -57,7 +57,9 @@ export default class Header extends React.Component<void, Props, State> {
         }}>
           <Toolbar>
             <IconButton color='contrast' onClick={closeOrOpenDrawer} aria-label='Navigation Menu'><MenuIcon /></IconButton>
-            <img src='/static/favicon.ico' style={{ marginRight: 10, marginLeft: 10, height: 40 }} alt='' />
+            <Link href='/' prefetch>
+              <img src='/static/favicon.ico' style={{ marginRight: 10, marginLeft: 10, height: 40 }} alt='' />
+            </Link>
             <Text type='title' color='inherit'>RSG-Group</Text>
             <div style={{flex: 1}} />
             <Button
