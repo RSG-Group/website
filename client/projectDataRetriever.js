dc41a5f2c44a0e4a067b89519a2db27539464dc4
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-09-10 16:53:25 +0530

    Need to add buttons functionality and types. Else we're done here.

--- client/projectDataRetriever.js
+++ client/projectDataRetriever.js
@@ -1,14 +1,68 @@
+// @flow
 // Import React.
 import React from 'react'
 
 // Query our Apollo Server w/ React Apollo.
 import { graphql, gql } from 'react-apollo'
 
+// Get Material-UI.
+import { Typography as Text, Grid, Card, CardContent, CardMedia, CardActions } from 'material-ui'
+
+// Types.
+type Props = {
+  allCardsQuery: {
+    loading?: boolean,
+    error?: string,
+    allCards: Array<{
+      image: Array<string>,
+      title: string,
+      description: string
+    }>
+  }
+}
+
 // Create a React Component.
-class Projects extends React.Component {
+class Projects extends React.Component<void, Props, void> {
   render () {
+    // If we are loading data or experienced an error.
+    if (this.props.allCardsQuery && this.props.allCardsQuery.loading) {
+      return <Text type='title'>Loading.</Text>
+    } else if (this.props.allCardsQuery && this.props.allCardsQuery.error) {
+      const error = this.props.allCardsQuery.error
+      return <Text type='title'>Error while loading data ({error}). Try reloading the page.</Text>
+    }
+    // If we've finished loading without errors, here we go.
+    const allCards = this.props.allCardsQuery.allCards
     return (
-      <div>Hullo.</div>
+      <Grid container>
+        {allCards.map(card => (
+          this.processCard(card)
+        ))}
+      </Grid>
+    )
+  }
+
+  processCard (card) {
+    // If Image is there..
+    const Image = () => card.image ? (
+      <CardMedia style={{ textAlign: 'center' }}>
+        <img
+          src={card.image[0]}
+          alt={card.image[1]}
+        />
+      </CardMedia>
+    ) : ''
+    // Return a card.
+    return (
+      <Card raised>
+        {card.image ? [<br />, <br />, <br />] : ''}
+        {card.image ? <Image /> : ''}
+        <CardContent>
+          <Text type='headline' component='h2'>{card.title}</Text>
+          <Text component='p'>{card.description}</Text>
+        </CardContent>
+        <CardActions />
+      </Card>
     )
   }
 }
@@ -29,4 +83,4 @@ const CARDS_QUERY = gql`
 `
 
 // Make the query and tranform our component into a container.
-export default graphql(CARDS_QUERY, { name: '' })(Projects)
+export default graphql(CARDS_QUERY, { name: 'allCardsQuery' })(Projects)
