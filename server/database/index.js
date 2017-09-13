81b0b91902a1a9237eab32507747dd5e28958f3f
Radi Cholakov <radicho123@gmail.com>
2017-09-13 10:40:53 +0300

    Added internalLink & divSpacing to the DB and image is an array.

--- server/database/index.js
+++ server/database/index.js
@@ -7,83 +7,116 @@ module.exports = [
     id: 1,
     title: 'RSG-Components',
     description: 'Some React components with beautiful styling.',
-    image: '/static/rsg-components.png',
+    image: [
+      '/static/rsg-components.png',
+      'A button in RSG-Components.'
+    ],
     controls: [{
       id: 1,
       title: 'Readme',
-      link: 'https://github.com/RSG-Group/rsg-components#RSG-Components'
+      link: 'https://github.com/RSG-Group/rsg-components#RSG-Components',
+      internalLink: false
     }, {
       id: 2,
       title: 'Repository',
-      link: 'https://github.com/RSG-Group/rsg-components'
+      link: 'https://github.com/RSG-Group/rsg-components',
+      internalLink: false
     }, {
       id: 3,
       title: 'Wiki',
-      link: 'https://github.com/RSG-Group/rsg-components/wiki'
-    }]
+      link: 'https://github.com/RSG-Group/rsg-components/wiki',
+      internalLink: false
+    }],
+    divSpacing: 26
   }, {
     id: 2,
     title: 'hyper-background',
     description: 'Change the background of your Hyper terminal!',
-    image: '/static/hyper-background.png',
+    image: [
+      '/static/hyper-background.png',
+      'Hyper with hyper-aurora theme and a background added via hyper-background. Pure ZSH theme in use.'
+    ],
     controls: [{
       id: 1,
       title: 'Readme',
-      link: 'https://github.com/RSG-Group/hyper-background#hyper-background'
+      link: 'https://github.com/RSG-Group/hyper-background#hyper-background',
+      internalLink: false
     }, {
       id: 2,
       title: 'Repository',
-      link: 'https://github.com/RSG-Group/hyper-background'
-    }]
+      link: 'https://github.com/RSG-Group/hyper-background',
+      internalLink: false
+    }],
+    divSpacing: 15
   }, {
     id: 3,
     title: 'Chess',
     description: 'Cool chess game to play with your friends. (Single player mode WIP)',
-    image: '/static/chess.png',
+    image: [
+      '/static/chess.png',
+      'Chess demo.',
+      '105pt'
+    ],
     controls: [{
       id: 1,
       title: 'Readme',
-      link: 'https://github.com/RSG-Group/Chess#rsg-chess'
+      link: 'https://github.com/RSG-Group/Chess#rsg-chess',
+      internalLink: false
     }, {
       id: 2,
       title: 'Repository',
-      link: 'https://github.com/RSG-Group/Chess'
+      link: 'https://github.com/RSG-Group/Chess',
+      internalLink: false
     }, {
       id: 3,
       title: 'Demo',
-      link: '/chess'
+      link: '/chess',
+      internalLink: true
     }]
   }, {
     id: 4,
     title: 'line-intersection',
     description: 'Simple mini library for finding the intersection of a line or line segment.',
-    image: '/static/line-intersection.png',
+    image: [
+      '/static/line-intersection.png',
+      'Rays bro. Rays.',
+      '105pt'
+    ],
     controls: [{
       id: 1,
       title: 'Readme',
-      link: 'https://github.com/RSG-Group/rsg-group.github.io/tree/master/intersection#line-intersection'
+      link: 'https://github.com/RSG-Group/rsg-group.github.io/tree/master/intersection#line-intersection',
+      internalLink: false
     }, {
       id: 2,
       title: 'Repository',
-      link: 'https://github.com/RSG-Group/rsg-group.github.io/tree/master/intersection'
+      link: 'https://github.com/RSG-Group/rsg-group.github.io/tree/master/intersection',
+      internalLink: false
     }]
   }, {
     id: 5,
     title: 'RSG-Chess-mobile',
     description: 'Mobile version of RSG-Chess.',
-    image: '/static/rsg-chess-mobile.png',
+    image: [
+      '/static/rsg-chess-mobile.png',
+      'RSG-Chess-mobile screenshot.'
+    ],
     controls: [{
       id: 1,
       title: 'Readme',
-      link: 'https://github.com/RSG-Group/RSG-Chess-mobile#RSG-Chess-mobile'
+      link: 'https://github.com/RSG-Group/RSG-Chess-mobile#RSG-Chess-mobile',
+      internalLink: false
     }, {
       id: 2,
       title: 'Repository',
-      link: 'https://github.com/RSG-Group/RSG-Chess-mobile'
+      link: 'https://github.com/RSG-Group/RSG-Chess-mobile',
+      internalLink: false
     }, {
       id: 3,
       title: 'Installers',
-      link: 'https://build.phonegap.com/apps/2773889/builds'
-    }]
+      link: 'https://build.phonegap.com/apps/2773889/builds',
+      internalLink: false
+    }],
+    divSpacing: 11
   }
 ]
