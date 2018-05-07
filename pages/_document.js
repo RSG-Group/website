ef42014df18e3fb6dcb811077dda15c185a38e51
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2018-05-07 12:57:32 +0530

    Updated packages, more reliable style rendering.

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,36 @@
 // ./pages/_document.js
-import Document, { Head, Main, NextScript } from 'next/document'
-import flush from 'styled-jsx/server'
 import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+import JssProvider from 'react-jss/lib/JssProvider'
+import flush from 'styled-jsx/server'
+import getPageContext from '../client/getPageContext'
 
-export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
-    const { html, head, errorHtml, chunks } = renderPage()
-    const styles = flush()
-    return { html, head, errorHtml, chunks, styles }
-  }
-
+class MyDocument extends Document {
   render () {
+    const { pageContext } = this.props
+
     return (
-      <html lang='en'>
+      <html lang='en' dir='ltr'>
         <Head>
-          <link media='print' rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <meta name='theme-color' content='#283593' />
           <link rel='icon' href='/static/favicon.ico' alt='' />
           <link rel='manifest' href='/static/manifest.json' />
+          <meta charSet='utf-8' />
+          {/* Use minimum-scale=1 to enable GPU rasterization */}
+          <meta
+            name='viewport'
+            content={
+              'user-scalable=0, initial-scale=1, ' +
+              'minimum-scale=1, width=device-width, height=device-height'
+            }
+          />
+          {/* PWA primary color */}
+          <meta name='theme-color' content={pageContext.theme.palette.primary.main} />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500'
+          />
         </Head>
         <body>
-          {this.props.customValue}
           <Main />
           <NextScript />
         </body>
@@ -29,3 +38,50 @@ export default class MyDocument extends Document {
     )
   }
 }
+
+MyDocument.getInitialProps = ctx => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. page.getInitialProps
+  // 2. document.getInitialProps
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the server with error:
+  // 2. document.getInitialProps
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. page.getInitialProps
+  // 3. page.render
+
+  // Get the context of the page to collected side effects.
+  const pageContext = getPageContext()
+  const page = ctx.renderPage(Component => props => ( // eslint-disable-line react/display-name
+    <JssProvider
+      registry={pageContext.sheetsRegistry}
+      generateClassName={pageContext.generateClassName}
+    >
+      <Component pageContext={pageContext} {...props} />
+    </JssProvider>
+  ))
+
+  return {
+    ...page,
+    pageContext,
+    styles: (
+      <React.Fragment>
+        <style
+          id='jss-server-side'
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: pageContext.sheetsRegistry.toString() }}
+        />
+        {flush() || null}
+      </React.Fragment>
+    )
+  }
+}
+
+export default MyDocument
